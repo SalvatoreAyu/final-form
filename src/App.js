fead3ccb45740f8a30f478972fa86d2d9500c59d
SalvatoreAyu <1446484292@qq.com>
2022-03-03 16:18:57 +0800

    hook3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Form from './components/simple'
 import Hook from './components/hook'
 function App() {
-  console.log('test hook hook2');
+  console.log('test hook hook2 3');
   return (
     <div className="App">
       <Form></Form>
